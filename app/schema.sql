a8359d94be59da0df4a722e1ed4c5b146efc39c3
Tomáš Řehák <tomrehak353@gmail.com>
2023-11-29 22:28:59 +0100

    SQL schema pro učitele a tagy

--- app/schema.sql
+++ app/schema.sql
@@ -1,4 +1,20 @@
-CREATE TABLE IF NOT EXISTS record (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS lecturers (
+    UUID TEXT UNIQUE NOT NULL,
+    title_before TEXT,
+    first_name TEXT,
+    middle_name TEXT,
+    last_name TEXT,
+    title_after TEXT,
+    picture_url TEXT,
+    location TEXT,
+    claim TEXT,
+    bio TEXT,
+    tags JSON DEFAULT('[]'),
+    price_per_hour INTEGER
+    emails JSON DEFAULT('[]'),
+    phone_numbers JSON DEFAULT('[]'),
+);
+CREATE TABLE IF NOT EXISTS tags (
+  UUID TEXT UNIQUE NOT NULL,
   name TEXT NOT NULL
 );
