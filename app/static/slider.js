8a77cdaf36b17e2b952dde1a0a196f7408a9d58e
GlaryCZ <kmatl@email.cz>
2024-02-25 16:54:18 +0100

    repaired slider

--- app/static/slider.js
+++ app/static/slider.js
@@ -1,13 +1,2 @@
 
-$(function () {
-$("#slider-range").slider({
-    range: true,
-    min: 0,
-    max: "{{max_price}}",
-values: [$('#amount-min').val(), $('#amount-max').val()],
-slide: function (event, ui) {
-    $("#amount-min").val(ui.values[0]);
-    $("#amount-max").val(ui.values[1]);
-}
-});
-} );
+
