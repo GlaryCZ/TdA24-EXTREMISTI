fff9c4dab533bf87c5c5ec4672e7e10c108607df
GlaryCZ <kmatl@email.cz>
2024-03-01 17:21:10 +0100

    import

--- app/app.py
+++ app/app.py
@@ -3,8 +3,8 @@ from typing import List, Dict, Tuple, Callable
 from uuid import uuid4 as make_uuid
 from hashlib import sha256
 from flask import Flask, jsonify, render_template, json, request, session, redirect, url_for
-import db
-import auto_maily
+from . import db
+from . import auto_maily
 
 # to .env TODO: novej tag schvali admin, max pocet tagu Bootstrap
 
